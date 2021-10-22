a9c7968c5d855367356e6cf4988a99224806cfe0
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2021-10-22 16:35:18 +0200

    Added result messages

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "The product has been added.";
+        public static string ProductNameInvalid = "Product name invalid";
+        public static string MaintenanceTime = "System is in maintenance.";
+        public static string ProductsListed = "Products are listed.";
+        public static string ProductFound = "The product has been found.";
+    }
+}
